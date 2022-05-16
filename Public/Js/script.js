125df54377dc5fc1ab2d0de16f429ffbed9eb5de
abdenacer <nasseressaouira@gmail.com>
2022-05-16 15:06:53 +0100

    Les requettes de Create is done

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -17,3 +17,15 @@ function openModalEdite(){
 function closeModalEdite(){
   document.getElementById("modal_student_Edite").style.display = "none"; 
 }
+
+
+let form = document.getElementById('form');
+let nom = document.getElementById('nom');
+let error = document.getElementById('error');
+
+form.addEventListener('submit', (e) =>{
+  if(nom.ariaValueMax.trim()){
+    error.textContent = "nom is empty"
+    nom.style.border = '1px solid red'
+  }
+})
