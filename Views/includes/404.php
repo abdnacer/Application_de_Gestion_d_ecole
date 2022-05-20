2ea171d751887c3af7fc8a7596f93bf9e17b41e1
safabalo <safaabalha58@gmail.com>
2022-05-20 08:45:36 +0000

    error

--- Views/includes/404.php
+++ Views/includes/404.php
@@ -1,1 +1,13 @@
-ERROR 404 NOT FOUND
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+    
+</head>
+<body>
+    <img src="Public/images/animation_500_l3e6pik3.gif" alt="Error">
+</body>
+</html>
